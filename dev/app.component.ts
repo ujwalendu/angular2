754b60c9eadbda74b215d0c8573da8d9ab294b07
Ujwalendu Prakash <ujwalendu@gmail.com>
2016-02-02 15:57:35 +0530

    Added contact list page. Fixed a bug of *ngFor

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -3,43 +3,49 @@ import {
   View
 } from 'angular2/core'
 
-import { 
+import {
   CORE_DIRECTIVES,
   FORM_DIRECTIVES
 } from 'angular2/common'
-  
+
 import {ContactDetail} from './contact-detail.component'
 
 @Component({
     selector: 'app',
-    template: `
-        <div (click)="clicked(contact)" *ngFor="#contact of getContacts()">
-            <p>{{contact.greetedName()}}</p>
-            <p>Email: {{contact.email}}</p>
-            <p>Phone: {{contact.phone}}</p>
-        </div>  
-    `,
+    templateUrl:'dev/contact-list.html'
 })
 
 export class AppComponent {
 
     public contacts = [];
-     
-    public getContacts() {
-        
-        this.contacts.push(
-            new ContactDetail("parth","parth@parth.com","9876543210")
-        );
-        
-        // this.contacts.push(
-        //     new ContactDetail("parth1","parth1@parth.com","9876543210")
-        // );
-        
+    public selectedContact;
 
-        return this.contacts;
+    constructor() {
+
+        if(!localStorage) {
+            throw new Error("Localstorage not found");
+        }
+
+
+        this.contacts.push(
+            new ContactDetail("parth", "parth@example_abc.com", "9876543210")
+        );
+        this.contacts.push(
+            new ContactDetail("kumar", "kumar@example_abc.com", "9876543210")
+        );
+        this.contacts.push(
+            new ContactDetail("mahajan", "mahajan@example_abc.com", "9876543210")
+        );
+        this.contacts.push(
+            new ContactDetail("shanket", "shanket@example_abc.com", "9876543210")
+        );
+        this.contacts.push(
+            new ContactDetail("swagat", "swagat@example_abc.com", "9876543210")
+        );
     }
-    
+
     clicked(contact) {
-        console.log(this.getContacts());
+        console.log(contact);
+        this.selectedContact = contact;
     }
 }
